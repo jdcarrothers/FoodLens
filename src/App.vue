4f06f10a5a71f90b5a45e0377ebd63aea6598687
Joshua Carrothers <jdcarrothers@icloud.com>
2023-12-15 14:32:19 +0000

    fully functional upload & analysis

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,82 @@
 <template>
-  <h1 class="title">Ingredient to meal</h1>
-  <button @click="uploadImage" class="submit-button">Upload Image</button>
-
+  <div class="container">
+    <p v-if="loading" class="title">Loading Response......</p>
+    <p v-else class="confirmation-text">{{ confirmationMessage }}</p>
+    <input type="file" @change="onFileChange" class="submit-button">Upload image>
+    <img :src="image" style="max-width: 200px;">
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  components: {
-    imageFile: "",
+  data() {
+    return {
+      imageFile: "",
+      confirmationMessage: "" ,
+      image: null,
+      loading: false,
+    };
   },
   methods: {
-    uploadImage(imageFile) {
-    
-    }
+      sendURL(image) {
+      //send image path to api 
+      this.loading = true;
+      fetch("http://127.0.0.1:5000/capture_and_save_frame", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          imageFile: image
+        }),
+      })
+      //display the response from the API
+        .then(response => response.json())
+        .then(data => {
+          this.loading = false;
+          this.confirmationMessage = "message sent successfully! The API responded with: " + data.result;
+        })
+        .catch(error => {
+          this.loading = false;
+          console.error("Error:", error);
+        });
+    },
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.previewImage(file);
+    },
+    previewImage(file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.image = e.target.result;
+        this.sendURL(e.target.result)
+      };
+      reader.readAsDataURL(file);
+      
+    },
   }
 }
 
 </script>
 <style scoped>
+.confirmation-text {
+  color: white;
+  font-size: x-large;
+}
+.container {
+  font-size: 2em;
+  color: white;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  height: 200vw;
+  width: 100vw;
+  background: linear-gradient(to bottom, #000000, #333333);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,7 +87,7 @@ export default {
 }
 .title {
   font-size: x-large;
-  color: rgb(0, 0, 0);
+  color: rgb(235, 235, 235);
   margin-bottom: 20px;
   background: linear-gradient(to bottom, #000000, #333333);
   -webkit-background-clip: text;
@@ -40,8 +97,8 @@ export default {
   padding: 10px 20px;
   border: none;
   border-radius: 100px;
-  background-color: #000000;
-  color: rgb(255, 252, 252);
+  background-color: #eee3e3;
+  color: rgb(2, 1, 1);
   cursor: pointer;
   transition: background-color 0.3s ease;
   display: inline-flex;
--- src/openAI.py
+++ src/openAI.py
@@ -1,6 +1,7 @@
 import cv2
 import base64
 import os
+import shutil
 import requests
 from flask_cors import CORS
 import json
@@ -9,81 +10,104 @@ from flask import Flask,jsonify,request
 app = Flask(__name__)
 CORS(app)
 api_key = "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"
-
+@app.route ('/capture_and_save_frame', methods =['POST']) 
 def capture_and_save_frame():
-    cap = cv2.VideoCapture(2)
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        cv2.imshow('Webcam Preview', frame)
-        # Press 's' to save
-        if cv2.waitKey(1) & 0xFF == ord('s'):
-            resized_frame = cv2.resize(frame, (512, 512))
-            cv2.imwrite('captured_frame.jpg', resized_frame)
-            break
-    print(cap)
-    cap.release()
-    return 'captured_frame.jpg'
+    imagePath, response, status_code = fetchFromJson() # gets the imageurl from the front end
+    if imagePath:
+        print(imagePath)#prints in log
+        #enable the three commented methods for testing using image url relative to os system  
+        # new_path = saveImageLocal(imagePath)    #saves the image to the local src in order for it to be acsessed by the python script
+        # base64_image = encode_image(new_path)   #that new path gets incoded; allows chat gpt to process
+        ai_response = AIReply(imagePath)     #ai responds with an answer regarding a possible recipe
+        # removeImageLocal(new_path)              #removes the image after a response has been generated
+        return jsonify({'result': ai_response}), 200 
+
+    else:
+        return response, status_code
+    
+def removeImageLocal(path):
+    save_directory = 'src/'
+    filename = os.path.basename(path)
+    file_path = os.path.join(save_directory, filename)
+
+    if os.path.exists(file_path):
+        try:
+            os.remove(file_path)
+            print(f"Image removed successfully from {file_path}")
+            return True
+        except OSError as e:
+            print(f"Error removing image: {e}")
+            return False
+    else:
+        print(f"Image not found at {file_path}")
+        return False
+    
+def saveImageLocal(path):
+    save_directory = 'src/'
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+    filename = os.path.basename(path)
+    new_path = os.path.join(save_directory, filename)
+
+    try:
+        shutil.copy(path, new_path)
+        print(f"Image saved successfully to {new_path}")
+        return new_path
+    except IOError as e:
+        print(f"Error saving image: {e}")
+        return None
+def fetchFromJson():
+    try:
+        data = request.get_json()
+        imageFile = data.get('imageFile')
+        if imageFile is None:
+            return None, jsonify({'error': 'Missing the data'}), 400
+        print("Gathered image data")
+        return imageFile, None, 200
+    except Exception as e:
+        return None, jsonify({'error': str(e)}), 500
 
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
+    
+def AIReply(base64_image):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
 
-image_path = capture_and_save_frame()
-base64_image = encode_image(image_path)
-
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_key}"
-}
-
-payload = {
-    "model": "gpt-4-vision-preview",
-    "messages": [
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "What am i showing you will be ingredients, they will probably be random ingredients, provide a quick description of the ingredients you see and what you can make with them. You must come up with something that is a real recipe, but something that is possible to make with the ingredients you see."
-                },
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{base64_image}"
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What am i showing you will be ingredients, they will probably be random ingredients,start your response by provide a brief description of the ingredients you see and what you can make with them. You must come up with something that is a real recipe, but something that is possible to make with the ingredients you see. Keep the answer breif!"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"{base64_image}"
+                        }
                     }
-                }
-            ]
-        }
-    ],
-    "max_tokens": 300
-}
+                ]
+            }
+        ],
+        "max_tokens": 300
+    }
 
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-response_text = json.loads(response.text)
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    response_text = json.loads(response.text)
 
-if response_text.get('choices'):
-    ai_response = response_text['choices'][0]['message']['content']
-else:
-    ai_response = "No response received."
-print(ai_response)
-def wrap_text(text, width):
-    return textwrap.wrap(text, width=width)
+    if response_text.get('choices'):
+        ai_response = response_text['choices'][0]['message']['content']
+    else:
+        ai_response = "No response received."
+    print(ai_response)
+    return ai_response
 
-cap = cv2.VideoCapture(1)
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    height, width = frame.shape[:2]
-    wrapped_text = wrap_text(ai_response, int(width / 20))
-    for i, line in enumerate(wrapped_text):
-        cv2.putText(frame, line, (20, height - 20 * (len(wrapped_text) - i)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.imshow('Webcam Preview', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-os.remove(image_path)
+if __name__ == '__main__':
+    app.run(debug=True)
